1b4730fd574add515a5da56425b87ca584f8e125
Coki1111 <coki231111@gmail.com>
2024-08-07 22:49:02 +0800

    Delete Coki trial directory

--- Coki trial/js/script-main.js
+++
@@ -1,7 +0,0 @@
-const particlesJs = document.getElementById('particles-js');
-
-function loadParticles() {
-    particlesJS.load('particles-js', 'particles.json', function() {
-        console.log('particles.js loaded - callback');
-    });
-}
